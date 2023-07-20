33da970bc6dce8e42abc70158723ee29c8374a37
pbdanny <pb.danny@gmail.com>
2023-07-20 16:52:55 +0700

    conf read write

--- utils/files.py
+++ utils/files.py
@@ -23,17 +23,12 @@ class DBPath(_Path_):
         rm_first_5_str = str(self.as_posix())[5:]
         return str("dbfs:"+rm_first_5_str)
 
-
 def conf_reader(path):
-    f = open(path, "r")
-    mapper = json.loads(f.read())
-    f.close()
+    with open(path, "r") as inf:
+        mapper = json.loads(inf.read())
     return mapper
 
-def save(df, path, format="delta", mode="overwrite", overwriteSchema=None):
-    if overwriteSchema:
-        df.write.format(format).mode(mode).option("overwriteSchema", True).save(
-            path)
-    else:
-        df.write.format(format).mode(mode).save(path)
-    print(">>> Saved to {}".format(path))
+def conf_writer(mapper, path):
+    with open(path, "w") as outfile:
+        json.dump(mapper, outfile)
+    return

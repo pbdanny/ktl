67f07262bbcd1e35bf633e99e9ceb1c39db22f03
pbdanny <pbdanny>
2023-07-26 01:54:50 +0000

    update from source

--- utils/files.py
+++ utils/files.py
@@ -2,31 +2,36 @@ from pathlib import Path as _Path_, _windows_flavour, _posix_flavour
 import os
 import json
 
+
 class DBPath(_Path_):
-    
-    _flavour = _windows_flavour if os.name == 'nt' else _posix_flavour
-    
+
+    _flavour = _windows_flavour if os.name == "nt" else _posix_flavour
+
     def __init__(self, input_file):
         if "dbfs:" in input_file:
-            raise ValueError("DBPath accept only file API path style (path start with /dbfs/)")
+            raise ValueError(
+                "DBPath accept only file API path style (path start with /dbfs/)"
+            )
         super().__init__()
         return
-        
+
     def __repr__(self):
         return f"DBPath class : {self.as_posix()}"
-    
+
     def file_api(self):
         rm_first_5_str = str(self.as_posix())[5:]
-        return str("/dbfs"+rm_first_5_str)
-    
+        return str("/dbfs" + rm_first_5_str)
+
     def spark_api(self):
         rm_first_5_str = str(self.as_posix())[5:]
-        return str("dbfs:"+rm_first_5_str)
+        return str("dbfs:" + rm_first_5_str)
+
 
 def conf_reader(path):
     with open(path, "r") as inf:
         mapper = json.loads(inf.read())
     return mapper
+
 
 def conf_writer(mapper, path):
     with open(path, "w") as outfile:

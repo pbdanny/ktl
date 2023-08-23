40345d2751a2e92ce99c24a10495b466967d7f0c
pbdanny <pbdanny>
2023-08-23 09:55:17 +0000

    update from databrick

--- notebook/feature_migration.py
+++ notebook/feature_migration.py
@@ -29,14 +29,11 @@ conf_path = "../config/feature_migration.json"
 
 conf_mapper = files.conf_reader(conf_path)
 
-decision_date = conf_mapper["data"]["decision_date"]
-gap_decision_n_month = conf_mapper["data"]["gap_decision_n_month"]
-print(f"{decision_date} , with gap months from decision date to data end date {gap_decision_n_month}")
-
 # COMMAND ----------
 
 # decision date = latest data end date
-decision_date =  datetime.strptime(decision_date, '%Y-%m-%d').date()
+decision_date =  datetime.strptime(conf_mapper["data"]["decision_date"], '%Y-%m-%d').date()
+gap_decision_n_month = float(conf_mapper["data"]["gap_decision_n_month"])
 
 # timeframe_end = end of months-2 from decision date
 # timeframe_start = 1 year from timeframe_end
@@ -44,11 +41,12 @@ timeframe_end = decision_date - relativedelta(months=+float(gap_decision_n_month
 timeframe_start = (timeframe_end - relativedelta(months=+11)).replace(day=1)
 
 print(f"decision date : {decision_date}\ntxn start date : {timeframe_start}\ntxn end date : {timeframe_end}")
+print(f"with gap months from decision date to data end date {gap_decision_n_month}")
 print(f"gap days from decision - txn end : {(decision_date - timeframe_end).days}")
 print(f"gap days from txn start - txn end : {(timeframe_end - timeframe_start).days}")
 
 # Get week_id of time frame
-date_dim = spark.table('tdm.v_date_dim').select('week_id', 'date_id', 'period_id', 'quarter_id', 'promoweek_id')
+date_dim = spark.table('tdm.v_date_dim').select('week_id', 'date_id')
 
 start_week = date_dim.filter(F.col('date_id').between(timeframe_start, timeframe_end)).agg(F.min('week_id')).collect()[0][0]
 end_week = date_dim.filter(F.col('date_id').between(timeframe_start, timeframe_end)).agg(F.max('week_id')).collect()[0][0]
@@ -67,6 +65,17 @@ files.conf_writer(conf_mapper, conf_path)
 
 # COMMAND ----------
 
+<<<<<<< Updated upstream
+=======
+from src.utils import files
+
+conf_path = "../config/feature_migration.json"
+
+conf_mapper = files.conf_reader(conf_path)
+
+# COMMAND ----------
+
+>>>>>>> Stashed changes
 from src.data import snap_txn
 
 cc_txn = snap_txn.get_txn_cc_exc_trdr(spark, conf_path)

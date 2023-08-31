33b67e17542c306f5bd842091c03738a4d80a428
pbdanny <pb.danny@gmail.com>
2023-08-31 09:25:54 +0700

    add src.utils

--- src/features/tender.py
+++ src/features/tender.py
@@ -1,4 +1,4 @@
-from utils.logger import logger
+from src.utils.logger import logger
 from pyspark.sql import functions as F
 from pyspark.sql import Window
 from datetime import datetime, timedelta
